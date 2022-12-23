09c21ffb42b731169ea926539ca3adaae7d5cb27
Jia <pochialu@gmail.com>
2022-12-23 17:39:40 +0800

    建立 共用 Header, team 頁面

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { ProSidebarProvider } from "react-pro-sidebar";
 import Topbar from './scenes/global/Topbar'
 import SidebarList from './scenes/global/Sidebar'
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -33,7 +33,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
