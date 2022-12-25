4ea414e717cf659307d9a6a918b3c558aa1813fa
Jia <pochialu@gmail.com>
2022-12-25 23:47:38 +0800

    新增 GeographyChart 元件，geopraphy 頁面，地籍資料

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Bar from "./scenes/bar";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -42,7 +42,7 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
