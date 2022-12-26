f0f807b85ac8748abb21b5beddc4b147ede98f2a
Jia <pochialu@gmail.com>
2022-12-27 01:55:40 +0800

    優化 元件採用 lazy 載入

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { ColorModeContext, useMode } from './theme'
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import Topbar from './scenes/global/Topbar'
 import SidebarList from './scenes/global/Sidebar'
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Contacts from "./scenes/contacts";
-import Invoices from "./scenes/invoices";
-import Form from "./scenes/form";
-import Bar from "./scenes/bar";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar";
+import { lazyLoad } from './lazyLoad';
+
+const Team = lazyLoad("./scenes/team")
+const Contacts = lazyLoad("./scenes/contacts")
+const Invoices = lazyLoad("./scenes/invoices")
+const Form = lazyLoad("./scenes/form")
+const Bar = lazyLoad("./scenes/bar")
+const Line = lazyLoad("./scenes/line")
+const Pie = lazyLoad("./scenes/pie")
+const FAQ = lazyLoad("./scenes/faq")
+const Geography = lazyLoad("./scenes/geography")
+const Calendar = lazyLoad("./scenes/calendar")
 
 function App() {
   const [theme, colorMode] = useMode(0)
@@ -31,19 +33,21 @@ function App() {
           </ProSidebarProvider>
           <main className='content'>
             <Topbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />
-            </Routes>
+            <Suspense fallback={<div> Loading...</div>} >
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/geography" element={<Geography />} />
+              </Routes>
+            </Suspense>
           </main>
         </div>
       </ThemeProvider>
