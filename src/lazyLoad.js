f9eff9a08dc4acd66c55223b9df8940ce4fc145b
Jia <pochialu@gmail.com>
2023-01-03 23:57:35 +0800

    調整 lazyload async/await

--- src/lazyLoad.js
+++ src/lazyLoad.js
@@ -1,14 +1,14 @@
 import { lazy } from "react";
 
 export function lazyLoad(path, namedExport) {
-  return lazy(() => {
+  return lazy(async () => {
     const promise = import(path)
 
     if (namedExport == null) {
       return promise
     } else {
-      return promise.then(module => ({ default: module[namedExport] })
-      )
+      const module = await promise;
+      return ({ default: module[namedExport] });
     }
   })
 }
